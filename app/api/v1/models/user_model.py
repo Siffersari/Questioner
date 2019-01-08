90616ae7bff9255be621c4d9c1b18337e34e6eac
LeewelKarani <Leewelkarani@gmail.com>
2019-01-08 13:17:33 +0300

    [feature #163047205] Initialize user models

---
+++ app/api/v1/models/user_model.py
@@ -0,0 +1,15 @@
+# This array users, stores all registered users
+
+users = []
+
+
+class UserModels(object):
+    """
+    This class UserModels contains the methods used when 
+    interacting with user details
+    """
+
+    def __init__(self):
+        self.db = users
+
+    pass
