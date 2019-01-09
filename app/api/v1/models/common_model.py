f3425f947228147dadac1b320136d37d2d052389
LeewelKarani <Leewelkarani@gmail.com>
2019-01-09 11:43:01 +0300

    [feature #163047316] Add common methods model

---
+++ app/api/v1/models/common_model.py
@@ -0,0 +1,20 @@
+
+class CommonModels(object):
+    """ 
+    This class contains methods that are common to all other
+    models
+    """
+
+    def makeresp(self, payload, status_code):
+        """ Returns user details if found and message if not """
+       
+        if isinstance(payload, str):
+            return {
+                "status": status_code,
+                "error": payload
+            }
+
+        return {
+            "status": status_code,
+            "data": [payload]
+        }
