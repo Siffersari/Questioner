6efdc6e454da64204d76dc8eb2209e69086b8a65
LeewelKarani <Leewelkarani@gmail.com>
2019-01-09 21:41:35 +0300

    [feature #163048412] Register new blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from .api.v1.views.user_views import version1 as v1
-from .api.v1.views.meetup_views import version1 as qu
+from .api.v1.views.meetup_views import version1 as meets
+from .api.v1.views.question_views import version1 as ques
 
 
 def create_app():
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.register_blueprint(v1)
-    app.register_blueprint(qu)
+    app.register_blueprint(meets)
+    app.register_blueprint(ques)
+
     
     return app  
