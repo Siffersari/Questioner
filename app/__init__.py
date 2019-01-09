bdb440daf3fe21b1f33abf4c1ad125e6aac34d4c
LeewelKarani <Leewelkarani@gmail.com>
2019-01-09 11:41:58 +0300

    [feature #163047316] register question view blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .api.v1.views.user_views import version1 as v1
+from .api.v1.views.meetup_views import version1 as qu
 
 
 def create_app():
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.register_blueprint(v1)
+    app.register_blueprint(qu)
     
     return app  
