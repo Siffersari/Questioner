4ff65fc80239ac01031dc24d66ceba64cc8cd24e
LeewelKarani <Leewelkarani@gmail.com>
2019-01-10 07:48:57 +0300

    [finishes #163048661] Register blueprint for rsvp

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .api.v1.views.user_views import version1 as v1
 from .api.v1.views.meetup_views import version1 as meets
 from .api.v1.views.question_views import version1 as ques
+from .api.v1.views.rsvp_views import version1 as rsvps 
 
 
 def create_app():
@@ -10,6 +11,7 @@ def create_app():
     app.register_blueprint(v1)
     app.register_blueprint(meets)
     app.register_blueprint(ques)
+    app.register_blueprint(rsvps)
 
     
     return app  
