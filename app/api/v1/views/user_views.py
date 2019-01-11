3d650e241d9036f96ef88e0c3e2cc477b5c1933f
LeewelKarani <Leewelkarani@gmail.com>
2019-01-11 20:39:10 +0300

    [chore #163151681] Fix imports and add validation

--- app/api/v1/views/user_views.py
+++ app/api/v1/views/user_views.py
@@ -2,16 +2,18 @@ from flask import Flask, request, jsonify
 from .. import version1
 from .. models.user_model import UserModels
 from werkzeug.security import check_password_hash
+from werkzeug.exceptions import BadRequest
 
 db = UserModels()
 
 
 @version1.route("/users", methods=["GET"])
 def hello():
-    """ Returns 'hello world' """
+    """ List of all registered users """
     resp = db.fetch_users()
 
     return jsonify(resp)
+
 
 @version1.route("/auth/signup", methods=["POST"])
 def register_user():
@@ -22,13 +24,17 @@ def register_user():
 
     return jsonify(resp), 201
 
+
 @version1.route("/auth/login", methods=["POST"])
 def login_user():
     """ Logs in a registered user """
     data = request.get_json()
-
-    password = data["password"],
-    username = data["username"]
+    try:
+        password = data["password"],
+        username = data["username"]
+    except KeyError as p:
+        raise BadRequest(
+            "{} should be present in the provided data".format(p))
 
     resp = db.login_user(username, password[0])
 
