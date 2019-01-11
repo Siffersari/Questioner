1a103f9a8975b2cc6f8e45ad99c0521d2fc9f4df
LeewelKarani <Leewelkarani@gmail.com>
2019-01-11 14:07:07 +0300

    [finishes #163145177] Restructure code to remove duplication

--- app/api/v1/views/meetup_views.py
+++ app/api/v1/views/meetup_views.py
@@ -15,9 +15,8 @@ def post_new_meetup():
 @version1.route("/meetups/<int:meetup_id>", methods=["GET"])
 def fetch_specific_meetup(meetup_id):
     """ Fetches a specific meetup record """
-    
-    if db.fetch_specific_meetup(meetup_id):
-        return jsonify(db.fetch_specific_meetup(meetup_id)), db.fetch_specific_meetup(meetup_id)["status"]
+        
+    return jsonify(db.fetch_specific_meetup(meetup_id)), db.fetch_specific_meetup(meetup_id)["status"]
 
 
 @version1.route("/meetups/upcoming", methods=["GET"])
