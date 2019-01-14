8213f4f47dc599745e759ef89c4913e287fe8903
LeewelKarani <Leewelkarani@gmail.com>
2019-01-14 20:36:22 +0300

    [start #163199315] Add validators class

---
+++ app/api/v1/utils/validators.py
@@ -0,0 +1,10 @@
+from ..models.base_model import BaseModels
+
+
+class DataValidators(BaseModels):
+    """ 
+    This class DataValidators contain all them methods that
+    validate data all across the application
+    """
+
+    pass
