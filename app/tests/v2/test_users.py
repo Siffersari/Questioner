fd48f8dbb1eb3d0d9541e6a714114c7f33d6ac6d
LeewelKarani <Leewelkarani@gmail.com>
2019-01-15 21:29:08 +0300

    [start #163232348] Add tests for user endpoints

---
+++ app/tests/v2/test_users.py
@@ -0,0 +1,71 @@
+import unittest
+import json
+from ... import create_app
+from ... db_con import create_tables, destroy_database
+
+
+class TestUsers(unittest.TestCase):
+    """ Contains all tests for user cases """
+
+    def setUp(self):
+        """ Sets up what the test will need before it runs """
+
+        self.app = create_app("testing")
+        self.client = self.app.test_client()
+        self.db = create_tables(db_type="testing")
+
+        self.data = {
+            "firstname": "User",
+            "lastname": "Test",
+            "othername": "UserTest",
+            "email": "test@test.com",
+            "phoneNumber": "0712332112",
+            "username": "testuser",
+            "password": "P@5sword"
+        }
+
+    def register_user(self, path="/api/v2/auth/signup", data={}):
+        """ Registers a new user given data or default if not provided"""
+
+        if not data:
+            data = self.data 
+
+        response = self.client.post(path, data=json.dumps(
+            data), content_type="application/json")
+
+        return response
+
+    def login_user(self, path="/api/v2/auth/login", data={}):
+        """ Logs in a user if registered """
+
+        if not data:
+            data = self.data
+
+        response = self.client.post(path, data=json.dumps(
+            data), content_type="application/json")
+
+        return response
+
+    def test_register_user(self):
+        """ Tests for user registration cases """
+
+        new_user = self.register_user()
+
+        self.assertEqual(new_user.status_code, 201)
+
+    def test_login_user(self):
+        """ Test cases for login in a user """
+        dummy_user = self.register_user()
+
+        self.assertEqual(self.login_user().status_code, 200)
+
+    def tearDown(self):
+        """ Destroys set up data before running each test """
+        
+        destroy_database()
+        self.db.close()
+
+
+if __name__ == "__main__":
+
+    unittest.main()
