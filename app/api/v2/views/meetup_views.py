8a2716d7fad28ebb1796039dc1fa16b97e9b0f44
LeewelKarani <Leewelkarani@gmail.com>
2019-01-16 16:43:29 +0300

    [finishes #163220214] Add authorization and endpoint route

--- app/api/v2/views/meetup_views.py
+++ app/api/v2/views/meetup_views.py
@@ -7,6 +7,23 @@ from .. models.meetup_model import MeetupModels
 def post_new_meetup():
     """ Creates a new meetup record if details provided"""
 
+    header = request.headers.get("Authorization")
+
+    if not header:
+        return jsonify(
+            {"error": "This resource is secured. Please provide authorization header",
+             "status": 400}
+        ), 400
+
+    auth_token = header.split(" ")[1]
+
+    response = MeetupModels().validate_token_status(auth_token)
+
+    if isinstance(response, str):
+        return jsonify(
+            {"error": response,
+             "status": 400}
+        ), 400
     data = request.get_json()
 
     try:
@@ -19,16 +36,3 @@ def post_new_meetup():
     resp = MeetupModels(data).create_meetup()
 
     return jsonify(resp), resp["status"]
-
-@version2.route("/meetups/<int:meetup_id>", methods=["GET"])
-def fetch_specific_meetup(meetup_id):
-    """ Fetches a specific meetup record """
-        
-    return jsonify(MeetupModels().fetch_specific_meetup(meetup_id)), MeetupModels().fetch_specific_meetup(meetup_id)["status"]
-
-
-@version2.route("/meetups/upcoming", methods=["GET"])
-def fetch_upcoming_meetup():
-    """ Fetches all upcoming meetup records """
-   
-    return jsonify(MeetupModels().fetch_upcoming_meetups()), MeetupModels().fetch_upcoming_meetups()["status"]
