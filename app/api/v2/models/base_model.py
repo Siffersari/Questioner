bc70a99040bc1e14f72d7c6d529770c71dd58c77
LeewelKarani <Leewelkarani@gmail.com>
2019-01-16 21:19:06 +0300

    [bug #163264737] Fix sql queries all over basemodel

--- app/api/v2/models/base_model.py
+++ app/api/v2/models/base_model.py
@@ -1,7 +1,7 @@
 import os
-from ....db_con import create_tables
 from flask import current_app
 from datetime import datetime, timedelta
+from ..utils.sql_helpers import SqlHelper
 import jwt
 
 
@@ -30,71 +30,6 @@ class BaseModels(object):
             "data": payload
         }
 
-    def get_user_by_username(self, username):
-        """ Fetches a user's details from the database given a username """
-
-        database = create_tables()
-
-        cur = database.cursor()
-        cur.execute(
-            """ SELECT user_id ,firstname, lastname, password, registered_on FROM users WHERE username = '{}'; """.format(username))
-
-        data = cur.fetchone()
-
-        cur.close()
-
-        if not data:
-            return "User not Found"
-
-        return data
-
-    def get_username_by_id(self, user_id):
-        """ returns a username given the id """
-
-        try:
-            cur = create_tables().cursor()
-            cur.execute(
-                """ SELECT username FROM users WHERE user_id = %d;""" % (user_id))
-            data = cur.fetchone()
-            cur.close()
-
-            return data
-
-        except Exception:
-            return "Not Found"
-
-    def fetch_id_if_text_exists(self, item_name, text, table):
-        # select meetup_id from meetups where topic = 'This is topic';
-        singular = table[:-1] + '_id'
-
-        cur = create_tables().cursor()
-        cur.execute(""" SELECT {} FROM {} WHERE lower({}) = '{}'; """.format(
-            singular, table, item_name, text.lower()))
-        data = cur.fetchone()
-
-        if not data:
-            # no meetup or question found with that text
-            return " Text not found"
-
-        return data[0]
-
-    def fetch_details_by_id(self, item_name, item_id, table):
-        """ returns a username given the id """
-
-        try:
-            cur = create_tables().cursor()
-            cur.execute(
-                """ SELECT * FROM {} WHERE {} = {}; """.format(table, item_name, int(item_id)))
-            data = cur.fetchone()
-            cur.close()
-
-            #response = [item for item in data if item in required]
-
-            return data
-
-        except Exception:
-            return "Not Found"
-
     def check_is_error(self, data):
         """ Checks if data passed to it is of type string """
 
@@ -119,28 +54,10 @@ class BaseModels(object):
 
         return token
 
-    def check_blacklisted_user_token(self, token):
-        """ Accepts a token and checks validity """
-
-        dbconn = create_tables()
-
-        curr = dbconn.cursor()
-
-        query = """ SELECT * FROM blacklisted WHERE tokens = %s; """
-
-        curr.execute(query, [token])
-
-        blacklisted = curr.fetchone()
-
-        if blacklisted:
-            return True
-
-        return False
-
     def validate_token_status(self, token):
         """ Decodes a given token """
 
-        if self.check_blacklisted_user_token(token):
+        if SqlHelper().check_blacklisted_user_token(token):
             return "Token is no longer valid. Get a new one"
 
         try:
@@ -155,42 +72,3 @@ class BaseModels(object):
         except jwt.InvalidTokenError:
 
             return "This token is invalid"
-
-    def check_user_is_admin(self, user_id):
-        """ Checks if a user is an admin using the user_id """
-        database = create_tables()
-
-        cur = database.cursor()
-        cur.execute(
-            """ SELECT user_id FROM users WHERE roles = 'true'; """)
-
-        admins = cur.fetchall()
-
-        cur.close()
-
-        if not admins:
-            return "Administrators not Found"
-
-        if not int(user_id) in admins[0]:
-            return "This user doesn't have the priviledges for this action"
-
-        return user_id
-
-    def make_user_administrator(self, user_id):
-        """ Checks if a user is an admin using the user_id """
-        database = create_tables()
-
-        cur = database.cursor()
-        cur.execute(
-            """ UPDATE users SET roles = true WHERE user_id = %d RETURNING roles; """ % (int(user_id)))
-
-        admins = cur.fetchone()
-
-        database.commit()
-
-        cur.close()
-
-        if not admins:
-            return "Failed to add user as Admin"
-
-        return admins
