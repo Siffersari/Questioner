f281904e6cf18347c02b9732e92472fed6c1caba
LeewelKarani <Leewelkarani@gmail.com>
2019-01-17 16:09:54 +0300

    [Finishes #163290749] Remove version2 blueprints from develop branch

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,6 @@ from .api.v1.views.meetup_views import version1 as meets
 from .api.v1.views.question_views import version1 as ques
 from .api.v1.views.rsvp_views import version1 as rsvps
 
-from .api.v2.views.user_views import version2 as v2
-from .api.v2.views.meetup_views import version2 as meets2
-from .api.v2.views.question_views import version2 as ques2
-from .api.v2.views.rsvp_views import version2 as rsvps2
-
 
 def create_app(config_name="development"):
     app = Flask(__name__, instance_relative_config=True)
@@ -23,9 +18,4 @@ def create_app(config_name="development"):
     app.register_blueprint(ques)
     app.register_blueprint(rsvps)
 
-    app.register_blueprint(v2)
-    app.register_blueprint(meets2)
-    app.register_blueprint(ques2)
-    app.register_blueprint(rsvps2)
-    
     return app
