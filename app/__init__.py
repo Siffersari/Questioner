9ff6c4663490ed92d36a7ef4488ee371ef99dff8
LeewelKarani <Leewelkarani@gmail.com>
2019-01-17 08:15:37 +0300

    [start #163220609] Register rsvp endpoint blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from instance.config import app_config
 from .api.v2.views.user_views import version2 as v2
 from .api.v2.views.meetup_views import version2 as meets2
 from .api.v2.views.question_views import version2 as ques2
+from .api.v2.views.rsvp_views import version2 as rsvps2
 
 
 def create_app(config_name="development"):
@@ -15,5 +16,6 @@ def create_app(config_name="development"):
     app.register_blueprint(v2)
     app.register_blueprint(meets2)
     app.register_blueprint(ques2)
+    app.register_blueprint(rsvps2)
 
     return app
