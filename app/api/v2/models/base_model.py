33cac78db6bb0437b332c2e40510ae549d3241c6
LeewelKarani <Leewelkarani@gmail.com>
2019-01-19 07:24:09 +0300

    [Finishes #163341848] Fix failing travis tests

--- app/api/v2/models/base_model.py
+++ app/api/v2/models/base_model.py
@@ -61,7 +61,8 @@ class BaseModels(object):
             return "Token is no longer valid. Get a new one"
 
         try:
-            data = jwt.decode(token, str(os.getenv("SECRET_KEY")), algorithms="HS256")
+            data = jwt.decode(token, str(
+                os.getenv("SECRET_KEY")), algorithms="HS256")
 
             return data["sub"]
 
