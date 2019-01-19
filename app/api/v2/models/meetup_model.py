95d0013e8e4b3576d2b57f3e8431962954d49c6c
LeewelKarani <Leewelkarani@gmail.com>
2019-01-19 19:48:10 +0300

    [Feature #163221058] Add models for post images endpoint

--- app/api/v2/models/meetup_model.py
+++ app/api/v2/models/meetup_model.py
@@ -95,6 +95,7 @@ class MeetupModels(BaseModels):
             "location": meetup[4],
             "happeningOn": "{:%B %d, %Y %I:%M%p}".format(meetup[3]),
             "tags": meetup[6],
+            "images": meetup[5],
             "createdOn": meetup[7],
             "createdBy": username[0]
         }
@@ -152,3 +153,63 @@ class MeetupModels(BaseModels):
         self.sql.delete_meetup(meetup_id)
 
         return self.makeresp(["This meetup has been deleted successfully"], 200)
+
+    def post_images(self, meetup_id):
+        """ Adds images to a meetup record """
+
+        try:
+            images = self.meetup_details["images"]
+
+        except KeyError as key:
+            return self.makeresp("{} is a required field".format(key), 400)
+
+        isempty = DataValidators(
+            self.meetup_details).check_values_not_empty()
+
+        if isinstance(isempty, str):
+            return self.makeresp(isempty, 400)
+
+        meetup = self.sql.fetch_details_by_id(
+            "meetup_id", meetup_id, "meetups")
+
+        if not meetup:
+            return self.makeresp("Meetup not found", 404)
+
+        user = self.sql.get_username_by_id(
+            int(self.meetup_details["user"]))
+
+        if not user:
+            return self.makeresp("This user is not found", 404)
+
+        is_admin = self.sql.get_admin_user(self.meetup_details["user"])
+
+        if self.check_is_error(is_admin):
+            status = 403
+            if 'Administrator' in is_admin:
+                status = 404
+
+            return self.makeresp(is_admin, status)
+
+        updated_img = SqlHelper(self.meetup_details).get_images(meetup_id)
+
+        if self.check_is_error(updated_img):
+            updimages = SqlHelper(self.meetup_details).post_images(meetup_id)
+
+            return self.makeresp({
+                "meetup": meetup[0],
+                "topic": meetup[2],
+                "images": updimages
+            }, 201)
+
+        self.meetup_details["images"] = updated_img + \
+            self.meetup_details["images"]
+
+        updated_img = SqlHelper(self.meetup_details).post_images(meetup_id)
+
+        response = {
+            "meetup": meetup[0],
+            "topic": meetup[2],
+            "images": self.meetup_details["images"]
+        }
+
+        return self.makeresp(response, 201)
